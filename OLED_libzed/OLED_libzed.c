198c77b8cc55f5dabcdcc54616194ab11b44086f
Thane <thanegallo@gmail.com>
2024-03-26 20:14:09 -0400

    added letter a for testing #9

--- OLED_libzed/OLED_libzed.c
+++ OLED_libzed/OLED_libzed.c
@@ -3,8 +3,61 @@
 #include <libzed/zed_oled.h>
 
 
-void drawLetter(){
-    
+void drawLetter(struct zedoled_data* inst, char letter, int offset_x, int offset_y){
+    switch(letter)
+    {
+        case a:
+        zedoled_set_pixel(struct zedoled_data* inst, 1 + offset_x, 0 + offset_y, 1);
+        zedoled_set_pixel(struct zedoled_data* inst, 2 + offset_x, 0 + offset_y, 1);
+        
+        zedoled_set_pixel(struct zedoled_data* inst, 0 + offset_x, 1 + offset_y, 1);
+        zedoled_set_pixel(struct zedoled_data* inst, 3 + offset_x, 1 + offset_y, 1);
+        
+        zedoled_set_pixel(struct zedoled_data* inst, 0 + offset_x, 2 + offset_y, 1);
+        zedoled_set_pixel(struct zedoled_data* inst, 1 + offset_x, 2 + offset_y, 1);
+        zedoled_set_pixel(struct zedoled_data* inst, 2 + offset_x, 2 + offset_y, 1);
+        zedoled_set_pixel(struct zedoled_data* inst, 3 + offset_x, 2 + offset_y, 1);
+        
+        zedoled_set_pixel(struct zedoled_data* inst, 0 + offset_x, 3 + offset_y, 1);
+        zedoled_set_pixel(struct zedoled_data* inst, 3 + offset_x, 3 + offset_y, 1);
+
+        zedoled_set_pixel(struct zedoled_data* inst, 0 + offset_x, 4 + offset_y, 1);
+        zedoled_set_pixel(struct zedoled_data* inst, 3 + offset_x, 4 + offset_y, 1);
+
+        case b:
+        
+        
+        case c:
+        
+        
+        case d:
+        
+        
+        case e:
+        
+        case f:
+        case g:
+        case h:
+        case i:
+        case j:
+        case k:
+        case l:
+        case m:
+        case n:
+        case o:
+        case p:
+        case q:
+        case r:
+        case s:
+        case t:
+        case u:
+        case v:
+        case w:
+        case x:
+        case y:
+        case z:
+
+    }
 }
 
 
