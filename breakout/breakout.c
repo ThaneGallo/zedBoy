1195a687e7b43a88ee9a6bd8cb3fad1476ae46f2
dmitmatv <dmitry.d.matveev@gmail.com>
2024-04-22 15:35:46 -0400

    change breakout  block count #17

--- breakout/breakout.c
+++ breakout/breakout.c
@@ -8,8 +8,8 @@
 
 #include "draw.h"
 
-#define WIDTH 128
-#define HEIGHT 32
+#define WIDTH 64
+#define HEIGHT 16
 #define PADDLE_WIDTH 8
 
 #define max(a, b) ((a) > (b) ? (a) : (b))
@@ -85,7 +85,7 @@ void setup()
     ball.y = HEIGHT - 3;
     ball.symbol = 'O';
 
-    int block_width = 6;
+    int block_width = 7;
     int block_height = 1;
     int padding = 1;
     int x_start = 2;
