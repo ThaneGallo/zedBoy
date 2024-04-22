cab6c2c45f45b122a7110c09bf2a8440bc580ac8
SymNC <sym.cunningham@gmail.com>
2024-04-22 14:06:41 -0400

    folder restructuring

--- zedboy/FrontEnd/zed_gaming_menu.c
+++ zedboy/FrontEnd/zed_gaming_menu.c
@@ -11,10 +11,10 @@
 #include <time.h>
 
 // there is a snake.c file in the a lower directory called snake/snake.c, also snake.h, include snake.h
-#include "utils/gameConstants.h"
-#include "games/snake/snake.h"
+#include "../utils/gameConstants.h"
+#include "../games/snake/snake.h"
 #include "zed_gaming_menu.h"
-#include "utils/displayImg.h"
+#include "../utils/displayImg.h"
 
 // example: LED GPIO Address
 #define SWITCH_GPIO_ADDR 0x41210000
--- zedboy/gameConstants.c
+++
@@ -1,8 +0,0 @@
-// gameConstants.c
-#include "gameConstants.h"
-
-int game_count = 0;
-int gameTick_count = 0;
-
-GameFunction games[MAX_GAMES];
-GameFunctionTick gameTicks[MAX_GAMES];
