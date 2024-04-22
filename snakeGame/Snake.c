3f88064365d11cf82d8e47e2bf08332faa176f6a
Thane <thanegallo@gmail.com>
2024-04-22 13:36:44 -0400

    fix header file

--- snakeGame/Snake.c
+++ snakeGame/Snake.c
@@ -12,6 +12,7 @@
 
 #include "draw.h"
 
+
 #define WIDTH 128
 #define HEIGHT 32
 
@@ -29,32 +30,32 @@ enum eDirection
 };
 enum eDirection dir;
 
-// int keyhit(void)
-// {
-//     struct termios oldt, newt;
-//     int ch;
-//     int oldf;
+int keyhit(void)
+{
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
 
-//     tcgetattr(STDIN_FILENO, &oldt);
-//     newt = oldt;
-//     newt.c_lflag &= ~(ICANON | ECHO);
-//     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-//     oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
-//     fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
-//     ch = getchar();
+    ch = getchar();
 
-//     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-//     fcntl(STDIN_FILENO, F_SETFL, oldf);
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-//     if (ch != EOF)
-//     {
-//         ungetc(ch, stdin);
-//         return 1;
-//     }
+    if (ch != EOF)
+    {
+        ungetc(ch, stdin);
+        return 1;
+    }
 
-//     return 0;
-// }
+    return 0;
+}
 
 void setup()
 {
